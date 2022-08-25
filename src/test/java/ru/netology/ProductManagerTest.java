0fb221e41fcbdc9b1aa5a10a8726b902cc34fbee
Elena Tkachenko <elena_20.09@inbox.ru>
2022-08-26 00:05:19 +0300

    fix 2

--- src/test/java/ru/netology/ProductManagerTest.java
+++ src/test/java/ru/netology/ProductManagerTest.java
@@ -9,6 +9,18 @@ import static org.junit.jupiter.api.Assertions.*;
 class ProductManagerTest {
 
     @Test
+    public void ShouldProductEmpty() {
+        Product[] expectedProduct = new Product[0];
+
+        ProductRepository repository = new ProductRepository();
+        ProductManager productManager = new ProductManager(repository);
+
+        Product[] foundedProduct = productManager.searchBy("book1");
+
+        assertArrayEquals(expectedProduct, foundedProduct);
+    }
+
+    @Test
     public void add() {
         Book book1 = new Book(1, "book1", 100, "Vasya Pupkin");
         Product[] expectedProduct = new Product[1];
@@ -43,6 +55,23 @@ class ProductManagerTest {
     }
 
     @Test
+    void shouldNotFound() {
+        Book book1 = new Book(1, "book1", 100, "Vasya Pupkin");
+        Smartphone phone1 = new Smartphone(1, "phone1", 200, "Apple");
+        Product[] expectedProduct = new Product[0];
+
+        ProductRepository repository = new ProductRepository();
+        ProductManager productManager = new ProductManager(repository);
+
+        productManager.add(book1);
+        productManager.add(phone1);
+
+        Product[] foundedProduct = productManager.searchBy("Garry Potter");
+
+        assertArrayEquals(expectedProduct, foundedProduct);
+    }
+
+    @Test
     void matches() {
         Book book1 = new Book(1, "book1", 100, "Vasya Pupkin");
         Smartphone phone1 = new Smartphone(1, "phone1", 200, "Apple");
@@ -56,6 +85,25 @@ class ProductManagerTest {
         productManager.add(phone1);
 
         Product[] foundedProduct = productManager.searchBy("phone1");
+
+        assertArrayEquals(expectedProduct, foundedProduct);
+    }
+
+    @Test
+    void shouldAllMatches() {
+        Book book1 = new Book(1, "book about phone", 100, "Vasya Pupkin");
+        Smartphone phone1 = new Smartphone(1, "phone", 200, "Apple");
+        Product[] expectedProduct = new Product[2];
+        expectedProduct[0] = book1;
+        expectedProduct[1] = phone1;
+
+        ProductRepository repository = new ProductRepository();
+        ProductManager productManager = new ProductManager(repository);
+
+        productManager.add(book1);
+        productManager.add(phone1);
+
+        Product[] foundedProduct = productManager.searchBy("phone");
 
         assertArrayEquals(expectedProduct, foundedProduct);
     }
